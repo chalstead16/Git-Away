1e13221674d893008ae83d9164eb02f440c42e31
Casey Halstead <catherineclosee@gmail.com>
2022-03-02 13:04:04 -0600

    Add Trip class

---
+++ src/classes/Trips.js
@@ -0,0 +1,13 @@
+class Trip {
+  constructor(tripRawData) {
+    this.id = tripRawData.id;
+    this.userID = tripRawData.userID;
+    this.travelers = tripRawData.travelers;
+    this.date = tripRawData.date;
+    this.duration = tripRawData.duration;
+    this.status = tripRawData.status;
+    this.suggestedActivities = tripRawData.suggestActivites;
+  }
+}
+
+export default Trip;
