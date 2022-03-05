d0571c6c05aabbe4ab6fdce3807aada012a25c00
Casey Halstead <catherineclosee@gmail.com>
2022-03-04 22:03:44 -0600

    Add fetch call for API data

---
+++ src/apiCalls.js
@@ -0,0 +1,7 @@
+const fetchData = (extension) => {
+  return fetch(`http://localhost:3001/api/v1/${extension}`)
+    .then(response => response.json())
+    .catch(err => console.log(err));
+};
+
+export default fetchData;
--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,27 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import fetchData from './apiCalls'
 import Traveler from './Traveler';
 import Trip from './Trip';
 import Destination from './Destination';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+//global variables
+let travelers;
+let traveler;
+let trips;
+let destinations;
+
+// functions
+const fetchAllData = () => {
+  Promise.all([fetchData('travelers'), fetchData('trips'), fetchData('destinations')])
+    .then(data => {
+      initializeData(data[0].travelers, data[1].trips, data[2].destinations);
+  });
+};
+
+const initializeData = (travelersRawData, tripsRawData, destinationsRawData) => {
+  travelers = travelersRawData.map(traveler => new Traveler(traveler));
+  trips = tripsRawData.map(trip => new Trip(trip));
+  destinations = destinationsRawData.map(destination => new Destination(destination));
+};
+
+fetchAllData()
