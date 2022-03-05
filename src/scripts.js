fdc8dbbfc9457dfc8575bc22c5837bb233b7d450
Casey Halstead <catherineclosee@gmail.com>
2022-03-05 08:36:28 -0600

    Add event listener for page load

--- src/scripts.js
+++ src/scripts.js
@@ -24,4 +24,5 @@ const initializeData = (travelersRawData, tripsRawData, destinationsRawData) => 
   destinations = destinationsRawData.map(destination => new Destination(destination));
 };
 
-fetchAllData()
+//event listeners
+window.addEventListener('load', fetchAllData)
