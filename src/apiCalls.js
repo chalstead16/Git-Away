9e5acf3e0b66f4d2342a45b913f3bc853f1a14b6
Casey Halstead <catherineclosee@gmail.com>
2022-03-05 20:11:02 -0600

    Add post request function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,4 +4,16 @@ const fetchData = (extension) => {
     .catch(err => console.log(err));
 };
 
-export default fetchData;
+const postData = (data, extension) => {
+  return fetch(`http://localhost:3001/api/v1/${extension}`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .catch(err => console.log(err));
+};
+
+export default {fetchData, postData};
