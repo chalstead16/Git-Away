a75de1932265d3347f47bbea24039489ce7c039f
Casey Halstead <catherineclosee@gmail.com>
2022-03-07 21:23:21 -0600

    Add method for displaying past trips in past trip section

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -4,11 +4,30 @@ let domUpdates = {
     welcome.innerText = `Welcome, ${traveler.name}`
   },
 
-  displayTravelerTrips(traveler) {
+  displayUpcomingTrips(traveler) {
     let displayTrips = document.querySelector('.js-trips');
     const presentOrFutureTrip = traveler.findPresentOrFutureTrips();
     presentOrFutureTrip.forEach(trip => {
       displayTrips.innerHTML += `
+      <article id="trip-card">
+        <h4>${trip.destination.destination}</h4>
+        <img class="destination" src="${trip.destination.image}" alt="${trip.destination.alt}">
+        <p>Date: ${trip.date}</p>
+        <p>Duration: ${trip.duration} day(s)</p>
+        <p>Total Traveler(s): ${trip.travelers}</p>
+        <p>Lodging: $${trip.destination.estimatedLodgingCostPerDay}</p>
+        <p>Flight: $${trip.destination.estimatedFlightCostPerPerson}</p>
+        <p>Status: ${trip.status}</p>
+      </article>
+      `
+    });
+  },
+
+  displayPendingTrips(traveler) {
+    const displayPendingTrips = document.querySelector('.js-pending-trips');
+    const pendingTrips = traveler.findPendingTrips()
+    pendingTrips.forEach(trip => {
+      displayPendingTrips.innerHTML += `
       <article id="trip-card">
         <h4>${trip.destination.destination}</h4>
         <img class="destination" src="${trip.destination.image}" alt="${trip.destination.alt}">
